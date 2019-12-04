8fcf11e04659710ace3bf90c69ad381b56f00afd
clavierbulb-green <clavierbulb-green@protonmail.com>
2019-12-03 16:10:20 -0800

    Add gameplay logic to buttons
    Can now play a full game of 5 rounds by selecting one's moves by
    pressing the corresponding button.
    
    game() removed; instead main game logic is handled by function attached
    to move buttons.

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,9 @@ const PLAYER_WIN = 1;
 const COMP_WIN = -1;
 const DRAW = 0
 
+let pScore = 0;
+let cScore = 0;
+let rounds = 0;
 
 function randomChoice(iterable) {
     /* Returns a random element from an iterable collection */
@@ -35,8 +38,7 @@ function computerPlay() {
 }
 
 function playRound(playerMove, computerMove) {
-    /* Plays a single round of Rock Paper Scissors; returns a string that 
-     * declares the winner of the round. */
+    /* Plays a single round of Rock Paper Scissors */
     
     let pIndex = MOVES.indexOf(playerMove);
     let cIndex = MOVES.indexOf(computerMove);
@@ -56,85 +58,98 @@ function playRound(playerMove, computerMove) {
 }
 
 
-function game(rounds) {
-    /* Plays a game of Rock Paper Scissors for a given number of rounds. */
-
-    //TODO set up DOM scoreboard
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let r = 1; r <= rounds; r++) {
-        //TODO change to update DOM banner
-        console.log(`Round ${r}:`)
-
-        let compMove = computerPlay();
-        let pMove = "";
-        while (!MOVES.includes(pMove)) {
-            //TODO change to a banner that moves in from the side of the screen,
-            //asking the player to select a move
-            pMove = prompt("Enter your move (rock, paper, or scissors)").toUpperCase();
-        }
-
-        let result = playRound(pMove, compMove);
-        switch (result) {
-            case PLAYER_WIN:
-                playerScore++;
-                console.log(`You WON this round! ${pMove} beats ${compMove}.`);
-                break;
-            case COMP_WIN:
-                computerScore++;
-                console.log(`You LOST this round! ${compMove} beats ${pMove}.`);
-                break;
-            default:
-                console.log(`This round was a DRAW.`);
-        }
-        //TODO set PLAYER_MOVE and COMP_MOVE to null
-        //TODO update DOM scoreboard instead
-        console.log(`PLAYER: ${playerScore}, COMPUTER: ${computerScore}`);
-        console.log(`\n`);
-    }
-
-    /* Print result of game */
-    if (playerScore > computerScore) {
-        console.log(`Congratulations! You won this game.`);
-    }
-    else if (playerScore < computerScore) {
-        console.log(`Sorry! You lost this game.`);
-    }
-    else {
-        console.log(`This game was a draw!`);
-    }
-}
-
-let playButtons = document.getElementsByClassName('play');
-for (let i = 0; i < playButtons.length; i++) {
-    let button = playButtons[i];
-    button.addEventListener('click', () => {
-        //TODO change to updating global PLAYER_MOVE?
-        let pMove = button.id;
-        let cMove = computerPlay();
-        console.log(playRound(pMove, cMove));
-    });
-}
-
 let startButton = document.getElementById("start");
 startButton.addEventListener("click", () => {
-    // hide dimmer and start dislpay (TODO put into separate function
+
+    // hide dimmer and start display (TODO put into separate function
     const dimmer = document.getElementById("dimmer");
     const newGameContainer = document.getElementById("new_game");
     dimmer.style.display = "none";
     newGameContainer.style.display = "none";
 
-    // TODO start a new game
 });
 
+function computeNewScore(pScore, cScore, result) {
+    /* Given a pair of player and computer scores, return a new pair of player 
+     * and computer scores that reflects the result of a given round of play. */
 
-function updateScore(pScore, cScore) {
-    /* Updates the DOM scoreboard with the player and computer scores */
+    let newPScore = pScore;
+    let newCScore = cScore;
+
+    switch (result) {
+        case PLAYER_WIN:
+            newPScore++;
+            break;
+        case COMP_WIN:
+            newCScore++;
+            break;
+    } 
+
+    return [newPScore, newCScore];
+}
+
+function updateScoreBoard(pScore, cScore) {
+    /* Updates the DOM scoreboard with the given player and computer scores */
 
     const pScoreDisplay = document.querySelector('#player ');
     const cScoreDisplay = document.querySelector('#computer'); 
 
     pScoreDisplay.textContent = pScore;
     cScoreDisplay.textContent = cScore;
+}
+
+
+let playButtons = document.getElementsByClassName('play');
+for (let i = 0; i < playButtons.length; i++) {
+    let button = playButtons[i];
+    button.addEventListener('click', () => {
+
+        let pMove = (button.id).toUpperCase();
+        let cMove = computerPlay();
+        let result = playRound(pMove, cMove);
+        
+        [pScore, cScore] = computeNewScore(pScore, cScore, result);
+        updateScoreBoard(pScore, cScore);
+        alertResult(pMove, cMove, result); //TODO
+        rounds++;
+
+        if (rounds === 5) {
+            alertVictory(pScore, cScore); //TODO
+            clear();
+        }
+    });
+}
+
+function clear() {
+    pScore = 0;
+    cScore = 0;
+    updateScoreBoard(pScore, cScore);
+    rounds = 0;
+}
+
+function alertResult(pMove, cMove, result) {
+    switch (result) {
+        case PLAYER_WIN:
+            alert(`You WON this round! ${pMove} beats ${cMove}.`);
+            break;
+        case COMP_WIN:
+            alert(`You LOST this round! ${cMove} beats ${pMove}.`);
+            break;
+        case DRAW:
+            alert(`This round was a DRAW.`);
+    }
+}
+
+function alertVictory(pScore, cScore) {
+    /* Print result of game */
+
+    if (pScore > cScore) {
+        alert(`Congratulations! You won this game.`);
+    }
+    else if (pScore < cScore) {
+        alert(`Sorry! You lost this game.`);
+    }
+    else {
+        alert(`This game was a draw!`);
+    }
 }
